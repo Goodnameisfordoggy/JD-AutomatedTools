5883ee3c1e160b39263491e01d46155cb084144f
Goodnameisfordoggy <h.dj.not.found@gmail.com>
2024-05-27 23:49:50 +0800

    add mysql_user.ini example

--- src/databaseManager.py
+++ src/databaseManager.py
@@ -1,3 +1,20 @@
+'''
+Author: HDJ
+StartDate: please fill in
+LastEditTime: 2024-05-27 23:44:37
+FilePath: \pythond:\LocalUsers\Goodnameisfordoggy-Gitee\jd-pers-data-exporter\src\DatabaseManager.py
+Description: 
+
+				*		写字楼里写字间，写字间里程序员；
+				*		程序人员写程序，又拿程序换酒钱。
+				*		酒醒只在网上坐，酒醉还来网下眠；
+				*		酒醉酒醒日复日，网上网下年复年。
+				*		但愿老死电脑间，不愿鞠躬老板前；
+				*		奔驰宝马贵者趣，公交自行程序员。
+				*		别人笑我忒疯癫，我笑自己命太贱；
+				*		不见满街漂亮妹，哪个归得程序员？    
+Copyright (c) 2024 by HDJ, All Rights Reserved. 
+'''
 import configparser
 import mysql.connector
 from mysql.connector import Error
