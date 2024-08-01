3b3d8c4e7e8357017cb21f720c8e3fe92c1c6654
Goodnameisfordoggy <h.dj.not.found@gmail.com>
2024-08-02 00:26:40 +0800

    add requirement.txt

--- JD-AutomaticEvaluate/main.py
+++ JD-AutomaticEvaluate/main.py
@@ -1,3 +1,20 @@
+'''
+Author: HDJ
+StartDate: please fill in
+LastEditTime: 2024-08-02 00:18:45
+FilePath: \pythond:\LocalUsers\Goodnameisfordoggy-Gitee\JD-Automated-Tools\JD-AutomaticEvaluate\main.py
+Description: 
+
+				*		写字楼里写字间，写字间里程序员；
+				*		程序人员写程序，又拿程序换酒钱。
+				*		酒醒只在网上坐，酒醉还来网下眠；
+				*		酒醉酒醒日复日，网上网下年复年。
+				*		但愿老死电脑间，不愿鞠躬老板前；
+				*		奔驰宝马贵者趣，公交自行程序员。
+				*		别人笑我忒疯癫，我笑自己命太贱；
+				*		不见满街漂亮妹，哪个归得程序员？    
+Copyright (c) 2024 by HDJ, All Rights Reserved. 
+'''
 import os
 import time
 import json
@@ -303,7 +320,7 @@ class AutomaticEvaluate():
             self.automaticEvaluate(
                 order_id=task['order_id'], url=task['orderVouche_url'], 
                 text_input=task['text_to_input'], image_files_path=task['image_path_to_input']) 
-            time.sleep(3)
+            time.sleep(10)
         self.logger.info('QwQ脚本运行结束，如有遗漏订单请再次运行！')
 
 if __name__ == '__main__':
--- JD-GetCookie/logInWithCookies.py
+++ JD-GetCookie/logInWithCookies.py
@@ -1,3 +1,20 @@
+'''
+Author: HDJ
+StartDate: please fill in
+LastEditTime: 2024-08-02 00:18:56
+FilePath: \pythond:\LocalUsers\Goodnameisfordoggy-Gitee\JD-Automated-Tools\JD-AutomaticEvaluate\logInWithCookies.py
+Description: 
+
+				*		写字楼里写字间，写字间里程序员；
+				*		程序人员写程序，又拿程序换酒钱。
+				*		酒醒只在网上坐，酒醉还来网下眠；
+				*		酒醉酒醒日复日，网上网下年复年。
+				*		但愿老死电脑间，不愿鞠躬老板前；
+				*		奔驰宝马贵者趣，公交自行程序员。
+				*		别人笑我忒疯癫，我笑自己命太贱；
+				*		不见满街漂亮妹，哪个归得程序员？    
+Copyright (c) 2024 by HDJ, All Rights Reserved. 
+'''
 import  os
 import time
 import json
@@ -18,7 +35,7 @@ def logInWithCookies(target_url: str = "https://www.jd.com/"):
     if os.path.exists(cookie_file): # 检查是否存在cookie文件
         driver.maximize_window()
         driver.get(target_url)
-        time.sleep(2)
+        # time.sleep(2)
         with open(cookie_file, 'r') as f:
             # 读取文件中的 cookie
             cookies = json.load(f)
@@ -26,9 +43,9 @@ def logInWithCookies(target_url: str = "https://www.jd.com/"):
             for cookie in cookies:
                 driver.add_cookie(cookie)
         logging.info('使用已保存的cookie登录')
-        time.sleep(1)
+        # time.sleep(1)
         driver.refresh()
-        time.sleep(2)
+        # time.sleep(2)
         return driver
         
 
